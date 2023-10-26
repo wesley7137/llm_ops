dabe026a816d685147a0d998dcf2702f846f039d
wesley7137 <132653112+wesley7137@users.noreply.github.com>
2023-10-26 17:13:10 -0500

    Update find_default_variables_to_run_main.py

--- misc/find_default_variables_to_run_main.py
+++ misc/find_default_variables_to_run_main.py
@@ -1,37 +1,8 @@
 from collections import defaultdict
 from pprint import pprint
 
-
-# Read the Python script into a list of lines
-with open('path/to/your/python_script.py', 'r') as f:
-    code_lines = f.readlines()
-
-# Initialize a dictionary to store function names and the line numbers where they are defined
-function_lines = {}
-
-# Loop through each line of code to find function definitions
-for idx, line in enumerate(code_lines):
-    line = line.strip()  # Remove leading and trailing whitespaces
-    if line.startswith("def "):
-        function_name = line.split('(')[0][4:]
-        function_lines[function_name] = idx
-
-# Your existing code starts here
-from collections import defaultdict
-call_relationships = defaultdict(list)
-
-for idx, line in enumerate(code_lines):
-    for function_name in function_lines.keys():
-        if function_name + '(' in line:
-            calling_function = None
-            for fn, line_num in function_lines.items():
-                if idx > line_num:
-                    calling_function = fn
-                else:
-                    break
-            if calling_function:
-                call_relationships[calling_function].append(function_name)
-
+# Your existing code to populate call_relationships
+# ...
 
 # Function to print relationships in a hierarchical manner
 def print_hierarchical_relationships(call_relationships, function_name, level=0):
@@ -49,14 +20,3 @@ print()
 print("Hierarchical Function Call Relationships:")
 for root_function in call_relationships.keys():
     print_hierarchical_relationships(call_relationships, root_function)
-This code snippet first uses pprint to display the call_relationships dictionary in a more readable format. After that, it uses the print_hierarchical_relationships function to display the relationships in a hierarchical manner.
-
-Just insert this snippet at the point in your code where you want to inspect the call_relationships dictionary. This should make it easier to understand the relationships between functions in your code.
-
-
-
-
-
-
-
-print(call_relationships)
